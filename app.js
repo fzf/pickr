60cf541bf29faf70360cb809511edc5752ababbb
Roy Pfaffman <rpfaffman@gmail.com>
2016-02-20 16:27:36 -0800

    Adjusts layout. Replaces images.

--- app.js
+++ app.js
@@ -3,33 +3,6 @@ angular
 .controller('cardsController', function ($scope) {
   $scope.cards = [
     {
-      "id": 132001,
-      "omdb_id": 137523,
-      "imdb_id": "tt0137523",
-      "title": "Fight Club",
-      "year": 1999,
-      "rating": "R",
-      "runtime": "139 min",
-      "genre": "Drama",
-      "released": "1999-10-15",
-      "director": "David Fincher",
-      "writer": "Chuck Palahniuk (novel), Jim Uhls (screenplay)",
-      "cast": "Edward Norton, Brad Pitt, Helena Bonham Carter, Meat Loaf",
-      "metacritic": 66,
-      "imdbRating": "8.8",
-      "imdbVotes": 1254308,
-      "poster": "http://ia.media-imdb.com/images/M/MV5BMjIwNTYzMzE1M15BMl5BanBnXkFtZTcwOTE5Mzg3OA@@._V1_SX300.jpg",
-       "plot": "An insomniac office worker, looking for a way to change his life, crosses paths with a devil-may-care soap maker, forming an underground fight club that evolves into something much, much more...",
-      "fullPlot": "A ticking-time-bomb insomniac and a slippery soap salesman channel primal male aggression into a shocking new form of therapy. Their concept catches on, with underground fight clubs forming in every town, until an eccentric gets in the way and ignites an out-of-control spiral toward oblivion.",
-      "language": "English",
-      "country": "USA, Germany",
-      "awards": "Nominated for 1 Oscar. Another 8 wins & 27 nominations.",
-       "lastUpdated": "2016-01-04T21:52:29.143Z",
-      "kind": "movie",
-      "created_at": "2016-01-21T09:34:36.882Z",
-      "updated_at": "2016-01-21T09:34:36.882Z"
-    },
-    {
       "id": 132002,
       "omdb_id": 137523,
       "imdb_id": "tt0137523",
